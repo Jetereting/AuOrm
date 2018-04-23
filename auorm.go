ba52a6af316c2dd2bf4c344fa9e5b28730556194
jetereting <1570967070@qq.com>
2018-04-23 12:02:12 +0800

    frist commit

---
+++ auorm.go
@@ -0,0 +1,42 @@
+package auorm
+
+import (
+	"database/sql"
+
+	"github.com/jinzhu/gorm"
+)
+
+// ResultMaps sql查询返回[]map[string]string类型
+func ResultMaps(db *gorm.DB, sqlQuery string, sqlValues ...interface{}) (result []map[string]string, err error) {
+	rows, err := db.Raw(sqlQuery, sqlValues...).Rows()
+	if err != nil {
+		return
+	}
+	cols, err := rows.Columns()
+	if err != nil {
+		return
+	}
+	values := make([]sql.RawBytes, len(cols))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return
+		}
+		var value string
+		resultC := map[string]string{}
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			resultC[cols[i]] = value
+		}
+		result = append(result, resultC)
+	}
+	return
+}
